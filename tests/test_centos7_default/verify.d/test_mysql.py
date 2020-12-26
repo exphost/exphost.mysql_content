e156fa391b2c6d5ceff62512db2a133d50a4d072
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2020-12-26 19:32:07 +0100

    add tests
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: I02b8be78e5f5b6a26d058a038f95faf7a8496fab
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>

---
+++ tests/test_centos7_default/verify.d/test_mysql.py
@@ -0,0 +1,12 @@
+def test_mysql_process(host):
+    assert host.service("my_db-mysql").is_running
+    assert host.service("my_db-mysql").is_enabled
+
+def test_check_mysql_access(host):
+    assert host.run("mysql --protocol=tcp -h 127.0.0.1 -u u1 -P 13306 -ppassword1 db1").succeeded
+    assert host.run("mysql --protocol=tcp -h 127.0.0.1 -u u1 -P 13306 -ppassword1 db2").failed
+    assert host.run("mysql --protocol=tcp -h 127.0.0.1 -u u1 -P 13306 -ppassword2 db1").failed
+
+    assert host.run("mysql --protocol=tcp -h 127.0.0.1 -u u3 -P 13306 -ppassword3 db1").succeeded
+    assert host.run("mysql --protocol=tcp -h 127.0.0.1 -u u3 -P 13306 -ppassword3 db2").succeeded
+    assert host.run("mysql --protocol=tcp -h 127.0.0.1 -u u3 -P 13306 -ppassword3 db3").failed
